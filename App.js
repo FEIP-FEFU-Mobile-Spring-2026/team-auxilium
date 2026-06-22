d97654813646bd54b2e5cd0fa85f4dc136a227e7
malivel <femershan@gmail.com>
2026-06-22 18:14:33 +1000

    Изменен App.js

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { ProductsProvider } from './src/context/ProductsContext';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <ProductsProvider>
-      <AppNavigator />
-    </ProductsProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ProductsProvider>
+        <AppNavigator />
+      </ProductsProvider>
+    </GestureHandlerRootView>
   );
 }
