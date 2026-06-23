848881b3f2e66bc2c80197bd820345e11fa61639
malivel <femershan@gmail.com>
2026-06-23 17:42:40 +1000

    Подключение CartProvider к App.js

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { ProductsProvider } from './src/context/ProductsContext';
+import { CartProvider } from './src/context/CartContext';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <ProductsProvider>
-        <AppNavigator />
+        <CartProvider>
+          <AppNavigator />
+        </CartProvider>
       </ProductsProvider>
     </GestureHandlerRootView>
   );
